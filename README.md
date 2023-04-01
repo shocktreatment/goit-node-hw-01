67c682ca20119ce525dab29eb12689a0a2cedd0a
shock_treatment <shock_treatment@icloud.com>
2023-04-01 12:48:26 +0300

    hw01

---
+++ README.md
@@ -0,0 +1,4 @@
+https://ibb.co/KFbqW2M
+https://ibb.co/hHDr1mm
+https://ibb.co/PZBkS4q
+https://ibb.co/1d2h8nc
---
+++ index.js
@@ -0,0 +1,37 @@
+const contacts = require("./db/contacts");
+
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+console.log("- - HW01 - -");
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const listContacts = await contacts.listContacts();
+      return console.table(listContacts);
+    case "get":
+      const oneContact = await contacts.getContactById(id);
+      return console.log(oneContact);
+    case "add":
+      const newContact = await contacts.addContact(name, email, phone);
+      return console.log(newContact);
+    case "remove":
+      const deleteContact = await contacts.removeContact(id);
+      return console.log(deleteContact);
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+invokeAction(argv);
